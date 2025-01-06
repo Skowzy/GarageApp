64358866bc8f5deed6e3ee0ea123699e8e914d16
Skowzy <trullu.sebastien@gmail.com>
2025-01-06 11:04:14 +0100

    update password modal

--- views/user/showOne.php
+++ views/user/showOne.php
@@ -55,6 +55,7 @@
 
 </main>
 
+<!--Edit modal-->
 <div id="editModal" class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog"
      aria-modal="true">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -102,8 +103,53 @@
         </div>
     </div>
 </div>
+<!--Password modal-->
+<div id="passwordModal" class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog"
+     aria-modal="true">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                    Modifier mes informations
+                </h3>
+                <div class="mt-2">
+                    <form method="post" action="?ctrl=user&action=update" id="editForm" class="space-y-4">
+                        <input type="hidden" name="id" value="<?= $user->getId() ?>">
+                        <div>
+                            <label for="oldPassword" class="block text-sm font-medium text-gray-700">Ancien mot de passe</label>
+                            <input type="password" id="oldPassword" name="oldPassword"
+                                   class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                        </div>
+                        <div>
+                            <label for="newPassword" class="block text-sm font-medium text-gray-700">Nouveau mot de passe</label>
+                            <input type="password" id="newPassword" name="newPassword"
+                                   class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                        </div>
+                        <div>
+                            <label for="confirmPassword" class="block text-sm font-medium text-gray-700">Confirmer le nouveau mot de passe</label>
+                            <input type="email" id="confirmPassword" name="confirmPassword"
+                                   class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                        </div>
+                        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                            <button type="submit"
+                                    class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+                                Sauvegarder
+                            </button>
+                            <button type="button"
+                                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                    onclick="closePasswordModal()">
+                                Annuler
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
-
+        </div>
+    </div>
+</div>
 <script>
     function openEditModal() {
         document.getElementById('editModal').classList.remove('hidden');
@@ -113,4 +159,11 @@
         document.getElementById('editModal').classList.add('hidden');
     }
 
+    function openPasswordModal() {
+        document.getElementById('passwordModal').classList.remove('hidden')
+    }
+
+    function closePasswordModal() {
+        document.getElementById('passwordModal').classList.add('hidden');
+    }
 </script>
